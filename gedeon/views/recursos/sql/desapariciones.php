a366bcf9ff5fc502e50141cc97384464c3bcb71d
Santiago Del Salto <santyrdse@gmail.com>
2020-02-26 00:26:44 -0500

    Added folders to easy changes on DB

---
+++ gedeon/views/recursos/sql/desapariciones.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="../assets/images/favicon-128x134.png" type="image/x-icon">
+  <meta name="description" content="">
+
+  <title>Desapariciones</title>
+  <link rel="preload" as="style" href="../assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="../assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="preload" as="style" href="../assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+  <link rel="preload" as="style" href="../assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="preload" as="style" href="../assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="../assets/tether/tether.min.css">
+  <link rel="stylesheet" href="../assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="../assets/animatecss/animate.min.css">
+  <link rel="stylesheet" href="../assets/theme/css/style.css">
+  <link rel="preload" as="style" href="../assets/gedeon/css/gdn-additional.css">
+  <link rel="stylesheet" href="../assets/gedeon/css/gdn-additional.css" type="text/css">
+
+  <link rel="stylesheet" href="../assets/social/social.css" type="text/css">
+  <link rel="stylesheet" href="../assets/icomoon/fuente_iconos.css">
+
+</head>
+
+<body>
+
+  <section id="social">
+    <div class="social">
+      <ul>
+        <li><a href="https://www.facebook.com/InvestigadoresPrivadosQuito/" target="_blank" class="icon-facebook"></a></li>
+        <li><a href="https://twitter.com/GedeonCia" target="_blank" class="icon-twitter"></a></li>
+        <li><a href="https://api.whatsapp.com/send?phone=593982467444&text=Hola,%20he%20visitado%20su%20sitio%20web,%20deseo%20mayor%20informaci%C3%B3n,%20%20mi%20consulta%20es:" target="_blank" class="icon-whatsapp"></a></li>
+        <li><a href="https://www.instagram.com/investigadores.ecuador/" target="_blank" class="icon-instagram"></a></li>
+        <li><a href="mailto:investigaciones.gedeonec@gmail.com" class="icon-mail2"></a></li>
+      </ul>
+    </div>
+  </section>
+  <section class="menu cid-rCDigoJUvs" once="menu" id="menu2-12">
+
+
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top collapsed bg-color transparent">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+      <div class="menu-logo">
+        <div class="navbar-brand">
+          <span class="navbar-logo">
+            <a href="../index.php">
+              <img src="../assets/images/logo-408x157.png" alt="Desapariciones" title="" style="height: 6.1rem;">
+            </a>
+          </span>
+
+        </div>
+      </div>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+          <li class="nav-item"><a class="nav-link link text-black display-4" href="../index.php">
+              Inicio
+            </a>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+  </section>
+
+  <section class="engine"><a href="../index.php">Programado por Fernando Chicaiza</a>
+  </section>
+  <section class="mbr-section content4 cid-rCDigq3bJu" id="content4-13">
+
+
+
+    <div class="container">
+      <div class="media-container-row">
+        <div class="title col-12 col-md-8">
+          <h2 class="align-center pb-3 mbr-fonts-style display-6">
+            <strong>Perm&#237tenos ayudarte&nbsp;</strong></h2>
+
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="mbr-section info3 cid-rCDigqQIEB" id="info3-14">
+
+
+
+    <div class="mbr-overlay" style="opacity: 0.7; background-color: rgb(35, 35, 35);">
+    </div>
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="media-container-column title col-12 col-md-10">
+          <h2 class="align-left mbr-bold mbr-white pb-3 mbr-fonts-style display-2">Personas desaparecidas</h2>
+          <h3 class="mbr-section-subtitle align-left mbr-light mbr-white pb-3 mbr-fonts-style display-5">
+            Nuestra experiencia en el &#225mbito de la investigaci&#243n privada nos permite obtener la mayor informaci&#243n posible al objeto de conocer las posibles causas de la desaparici&#243n. Nuestro equipo de detectives privados lleva a cabo las t&#233cnicas precisas de investigaci&#243n con el objetivo de determinar el paradero de la persona desaparecida as&#237 como la reconstrucci&#243n de los hechos acaecidos.
+
+
+          </h3>
+
+          <div class="mbr-section-btn align-left py-4"><a class="btn btn-success display-4" href="javascript:history.back()">REGRESAR</a>
+            <a class="btn btn-white-outline display-4" href="../index.php#formulario">CONTACTO</a></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <script src="../assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="../assets/popper/popper.min.js"></script>
+  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+  <script async src="../assets/tether/tether.min.js"></script>
+  <script async src="../assets/dropdown/js/nav-dropdown.js"></script>
+  <script async src="../assets/dropdown/js/navbar-dropdown.js"></script>
+  <script async src="../assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script async src="../assets/viewportchecker/jquery.viewportchecker.js"></script>
+  <script async src="../assets/smoothscroll/smooth-scroll.js"></script>
+  <script async src="../assets/theme/js/script.js"></script>
+
+
+  <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;">
+      <i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
+  <input name="animation" type="hidden">
+  <?php require './footer_content.php'; ?>
+
+</body>
+
+</html>
---
+++ gedeon/views/recursos/sql/familiar.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="../assets/images/favicon-128x134.png" type="image/x-icon">
+    <meta name="description" content="Investigación de conducta conyugal y actividades-seguimientos, conducta prematrimonial, obtencion de pruebas para separación y divorcio, investigacion de hábitos, amistades y entorno en el que se relacionan sus hijos.">
+
+    <title>Familiar</title>
+    <link rel="preload" as="style" href="../assets/web/assets/mobirise-icons/mobirise-icons.css">
+    <link rel="stylesheet" href="../assets/web/assets/mobirise-icons/mobirise-icons.css">
+    <link rel="preload" as="style" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="preload" as="style" href="../assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="preload" as="style" href="../assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="../assets/tether/tether.min.css">
+    <link rel="stylesheet" href="../assets/animatecss/animate.min.css">
+    <link rel="stylesheet" href="../assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="../assets/theme/css/style.css">
+    <link rel="preload" as="style" href="../assets/gedeon/css/gdn-additional.css">
+    <link rel="stylesheet" href="../assets/gedeon/css/gdn-additional.css" type="text/css">
+
+
+
+</head>
+
+<body>
+    <section class="menu cid-rC9z5kiS8T" once="menu" id="menu2-d">
+        <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top collapsed bg-color transparent" style="background-color: black">
+            <div class="menu-logo">
+                <div class="navbar-brand">
+                    <span class="navbar-logo">
+                        <a href="../index.php">
+                            <img src="../assets/images/logo-797x284.png" alt="Familiar" title="" style="height: 6.1rem;">
+                        </a>
+                    </span>
+
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section class="engine"><a href="../index.php">Programado por Fernando Chicaiza</a></section>
+
+    <section class="mbr-section content4 cid-rCD3oEFjgV" id="content4-u" style="background-color: black">
+        <div class="container">
+            <div class="media-container-row">
+                <div class="title col-12 col-md-8">
+                    <h2 class="align-center pb-3 mbr-fonts-style display-6" style="color: white">
+                        <strong>&#191Tienes dudas y no sabes que hacer ?</strong>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mbr-section info3 cid-rCD1zPlBn5 mbr-parallax-background" id="info3-t">
+
+
+
+        <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);">
+        </div>
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="media-container-column title col-12 col-md-10">
+                    <h2 class="align-left mbr-bold mbr-white pb-3 mbr-fonts-style display-2">
+                        &#193rea Familiar</h2>
+                    <h3 class="mbr-section-subtitle align-left mbr-light mbr-white pb-3 mbr-fonts-style display-5">
+                        <em>Investigaci&#243n de Conducta Conyugal y Actividades y/o Seguimientos, Conducta Prematrimonial, Obtenci&#243n de pruebas para Separaci&#243n y Divorcio, adem&#225s realizamos Investigaci&#243n de H&#225bitos, amistades y entorno en el que se relacionan sus hijos.</em></h3>
+
+                    <div class="mbr-section-btn align-left py-4"><a class="btn btn-success display-4" href="javascript:history.back()">REGRESAR</a>
+                        <a class="btn btn-white-outline display-4" href="../index.php#formulario">CONTACTO</a></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <script src="../assets/web/assets/jquery/jquery.min.js"></script>
+    <script src="../assets/popper/popper.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script async src="../assets/tether/tether.min.js"></script>
+    <script async src="../assets/smoothscroll/smooth-scroll.js"></script>
+    <script async src="../assets/touchswipe/jquery.touch-swipe.min.js"></script>
+    <script async src="../assets/viewportchecker/jquery.viewportchecker.js"></script>
+    <script async src="../assets/parallax/jarallax.min.js"></script>
+    <script async src="../assets/dropdown/js/nav-dropdown.js"></script>
+    <script async src="../assets/dropdown/js/navbar-dropdown.js"></script>
+    <script async src="../assets/theme/js/script.js"></script>
+
+
+    <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
+    <input name="animation" type="hidden">
+    <?php require '../views/footer_content.php'; ?>
+</body>
+
+</html>
